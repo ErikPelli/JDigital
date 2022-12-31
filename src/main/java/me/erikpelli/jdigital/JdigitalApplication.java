7a212b407334487383ef84d6d6a345352adbab27
ErikPelli <erikpellizzon1@gmail.com>
2022-12-31 14:49:51 +0100

    Initialize Spring Boot API with Maven

---
+++ src/main/java/me/erikpelli/jdigital/JdigitalApplication.java
@@ -0,0 +1,13 @@
+package me.erikpelli.jdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdigitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/erikpelli/jdigital/JdigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.erikpelli.jdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
