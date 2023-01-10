786c18e4a3ba9664623e99c06b6b0949b46063f0
ErikPelli <erikpellizzon1@gmail.com>
2023-01-10 20:26:42 +0100

    Create UserSettings Entity

---
+++ src/main/java/me/erikpelli/jdigital/user/settings/UserSettings.java
@@ -0,0 +1,59 @@
+package me.erikpelli.jdigital.user.settings;
+
+import jakarta.persistence.*;
+import me.erikpelli.jdigital.company.Company;
+
+@Embeddable
+public class UserSettings {
+    /**
+     * Employer of the current user.
+     */
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Company employer;
+
+    /**
+     * Job of the user (ceo, manager, employee, ecc.)
+     */
+    @Column(nullable = false)
+    private String job;
+
+    /**
+     * Role of the user inside the company (developer, accountant, ecc.)
+     */
+    @Column(nullable = false)
+    private String role;
+
+    protected UserSettings() {
+    }
+
+    public UserSettings(Company employer, String job, String role) {
+        this.employer = employer;
+        this.job = job;
+        this.role = role;
+    }
+
+    public String getEmployerCode() {
+        return employer.getVatNum();
+    }
+
+    public void setEmployer(Company employer) {
+        this.employer = employer;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
