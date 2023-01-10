8727f9674e4f46b539fa72c71bd0a1fcaa918150
ErikPelli <erikpellizzon1@gmail.com>
2023-01-10 20:27:21 +0100

    Implement API for UserSettings

---
+++ src/main/java/me/erikpelli/jdigital/user/settings/UserSettingsController.java
@@ -0,0 +1,80 @@
+package me.erikpelli.jdigital.user.settings;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import me.erikpelli.jdigital.user.User;
+import me.erikpelli.jdigital.user.UserViews;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+public class UserSettingsController {
+    private final UserSettingsService userSettingsService;
+
+    public UserSettingsController(UserSettingsService userSettingsService) {
+        this.userSettingsService = userSettingsService;
+    }
+
+    /**
+     * GET /api/settings
+     *
+     * @param userData email
+     * @return job, role, company
+     */
+    @GetMapping("/settings")
+    public Map<String, Object> getCurrentSettings(
+            @RequestBody(required = false)
+            @JsonView(UserViews.UserEmail.class)
+            User userData) {
+        var email = (userData != null) ? userData.getEmail() : null;
+        var settings = userSettingsService.getByEmail(email);
+        return Map.of(
+                "job", settings.getJob(),
+                "role", settings.getRole(),
+                "company", settings.getEmployerCode()
+        );
+    }
+
+    /**
+     * POST /api/settings
+     *
+     * @param settingsData email, [optional] job role company
+     */
+    @PostMapping("/settings")
+    public Object overwriteSomeSettings(
+            @RequestBody(required = false)
+            Map<String, String> settingsData) {
+        if (settingsData == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "required parameters are missing");
+        }
+        var email = settingsData.get("email");
+        var job = settingsData.get("job");
+        var role = settingsData.get("role");
+        var company = settingsData.get("company");
+        userSettingsService.setSettings(
+                email,
+                Optional.ofNullable(job),
+                Optional.ofNullable(role),
+                Optional.ofNullable(company)
+        );
+        return Map.of();
+    }
+
+    /**
+     * DELETE /api/settings
+     *
+     * @param userData email
+     */
+    @DeleteMapping("/settings")
+    public Object resetUserSettings(
+            @RequestBody(required = false)
+            @JsonView(UserViews.UserEmail.class)
+            User userData) {
+        var email = (userData != null) ? userData.getEmail() : null;
+        userSettingsService.resetSettings(email);
+        return Map.of();
+    }
+}
---
+++ src/main/java/me/erikpelli/jdigital/user/settings/UserSettingsService.java
@@ -0,0 +1,89 @@
+package me.erikpelli.jdigital.user.settings;
+
+import me.erikpelli.jdigital.company.CompanyRepository;
+import me.erikpelli.jdigital.user.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+public class UserSettingsService {
+    private final UserRepository userRepository;
+
+    private final CompanyRepository companyRepository;
+
+    @Value("${user.default_job}")
+    private String defaultJob;
+
+    @Value("${user.default_role}")
+    private String defaultRole;
+
+    @Value("${user.default_company}")
+    private String defaultCompany;
+
+    public UserSettingsService(UserRepository userRepository, CompanyRepository companyRepository) {
+        this.userRepository = userRepository;
+        this.companyRepository = companyRepository;
+    }
+
+    /**
+     * Get settings of a user from its email.
+     *
+     * @param email email of the user
+     * @return UserSettings object
+     */
+    public UserSettings getByEmail(String email) {
+        if (email == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "email is mandatory");
+        }
+        var user = userRepository.findFirstByEmail(email);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+        }
+        return user.getSettings();
+    }
+
+    /**
+     * Change the settings of a single user, empty optional parameters won't be changed.
+     *
+     * @param email   email of the user to change
+     * @param job     new user job
+     * @param role    new user role
+     * @param company new user company
+     */
+    public void setSettings(String email, Optional<String> job, Optional<String> role, Optional<String> company) {
+        if (email == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "email is mandatory");
+        }
+        var user = userRepository.findFirstByEmail(email);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+        }
+
+        var currentSettings = Optional.ofNullable(user.getSettings()).orElseGet(UserSettings::new);
+        job.ifPresent(currentSettings::setJob);
+        role.ifPresent(currentSettings::setRole);
+        if (company.isPresent()) {
+            var findCompany = companyRepository.findFirstByVatNum(company.get());
+            if (findCompany == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "company not found");
+            }
+            currentSettings.setEmployer(findCompany);
+        }
+
+        user.setSettings(currentSettings);
+        userRepository.save(user);
+    }
+
+    /**
+     * Reset the settings of a user with the default one.
+     *
+     * @param email email of the user to reset
+     */
+    public void resetSettings(String email) {
+        setSettings(email, Optional.of(defaultJob), Optional.of(defaultRole), Optional.of(defaultCompany));
+    }
+}
