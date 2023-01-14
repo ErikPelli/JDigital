6add6978946dfdae05b63e383bb057777d2e953e
ErikPelli <erikpellizzon1@gmail.com>
2023-01-14 17:44:29 +0100

    Add NonComplianceType entity that represents the type of noncompliance

---
+++ src/main/java/me/erikpelli/jdigital/noncompliance/type/NonComplianceType.java
@@ -0,0 +1,71 @@
+package me.erikpelli.jdigital.noncompliance.type;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class NonComplianceType {
+    /**
+     * Unique VAT Number.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer code;
+
+    /**
+     * Name of the non-compliance type.
+     */
+    @Column(nullable = false)
+    private String name;
+
+    /**
+     * Optional description of this type.
+     */
+    private String description;
+
+    protected NonComplianceType() {
+    }
+
+    public NonComplianceType(Integer code, String name, String description) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NonComplianceType that)) return false;
+        return name.equals(that.name) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description);
+    }
+}
